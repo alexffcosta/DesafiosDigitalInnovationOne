7653b3787ff7fada3adf8a86eb5f28e61716ec9b
alexffcosta <alexffcosta@gmail.com>
2021-08-01 22:00:41 -0300

    update arquivos

---
+++ DesafioDistancia/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioDistancia
+{
+    class Program
+    {
+    static void Main()
+            {
+                int quilometros = Int32.Parse(Console.ReadLine());
+                int minutos = quilometros * 2; // Digite aqui o calculo dos minutos
+                Console.WriteLine(minutos + " minutos");
+            Console.ReadKey();
+            }
+    }
+}
